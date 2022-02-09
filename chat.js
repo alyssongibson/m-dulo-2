07f53612b889c7c67a4849575a2ca8eb88d06766
alyssongibson <alyssongibson25@gmail.com>
2022-02-08 21:32:38 -0300

    criei uns comandos

---
+++ chat.js
@@ -0,0 +1,39 @@
+// const input = require("readline-sync");
+
+// console.log("seja bem vindo! Vamos fazer o seu longin")
+
+// const idade = input.question("Qual a sua idade");
+
+// if (idade >= 16 && idade < 18) {
+// console.log("pode entrar mas não pode beber")
+// } else if (idade >= 18) {
+//     console.log("Pode entrar e pode beber")
+// }else {
+//     console.log("entrada não permitida")
+// }
+
+// const input = require("readline-sync");
+
+// console.log("Boa noite. consulte seu resultado digitando sua nota")
+
+// const nota = input.question ("Qual foi sua nota?");
+// if (nota >= 6){
+//     console.log("Parabéns! aprovado")
+//     }else if (nota <6) {
+//         console.log("Lamento, você foi reprovado")
+//     }
+    
+// const nome= "arthur";
+// console.log (nome)
+// const tipo= "personagem, urso";
+// console.log (tipo)
+// const altura= "50cm";
+// console.log (altura)
+
+
+
+
+
+    
+ 
+
