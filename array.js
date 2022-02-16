3900b469d34f01838d8e2ce05675dbd0abe4551e
wilkerkennedy <wilkerkdsferreira@recife.pe.senac.br>
2022-02-15 21:19:26 -0300

    criei uma array com dados de Alunos

---
+++ array.js
@@ -0,0 +1,19 @@
+// const lista = new Array ('pera', 'uva', 'maça')
+
+// const primeiroItem = lista[0]
+
+// const tamanho = lista.length
+
+const Alunos = new Array ('Marcos', '18 anos', 'Pardo')
+
+const Aluno = [
+    {Nome: 'Márcia', Idade: '18 anos', Cor: 'Parda'},
+    {Nome: 'Flávio', Idade: '21 anos', Cor: 'Negro'},
+    {Nome: 'Tereza', Idade: '35 anos', Cor: 'Branca'}]
+
+const tamanho = Aluno.length
+console.log(Aluno.length)
+
+//const dados = Aluno[1]
+console.log(Aluno[0].Idade)
+
