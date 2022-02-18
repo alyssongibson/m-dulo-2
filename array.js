4f4ac74602714f9d63b8c69650efcf46d3c90b76
clear <alyssongibson25@gmail.com>
2022-02-17 21:33:33 -0300

    novo exercicio concluido

--- array.js
+++ array.js
@@ -4,16 +4,43 @@
 
 // const tamanho = lista.length
 
-const Alunos = new Array ('Marcos', '18 anos', 'Pardo')
+// const Alunos = new Array ('Marcos', '18 anos', 'Pardo')
 
-const Aluno = [
-    {Nome: 'Márcia', Idade: '18 anos', Cor: 'Parda'},
-    {Nome: 'Flávio', Idade: '21 anos', Cor: 'Negro'},
-    {Nome: 'Tereza', Idade: '35 anos', Cor: 'Branca'}]
+// const Aluno = [
+//     {Nome: 'Márcia', Idade: '18 anos', Cor: 'Parda'},
+//     {Nome: 'Flávio', Idade: '21 anos', Cor: 'Negro'},
+//     {Nome: 'Tereza', Idade: '35 anos', Cor: 'Branca'}]
 
-const tamanho = Aluno.length
-console.log(Aluno.length)
+// const tamanho = Aluno.length
+// console.log(Aluno.length)
 
-//const dados = Aluno[1]
-console.log(Aluno[0].Idade)
+// //const dados = Aluno[1]
+// console.log(Aluno[0].Idade)
 
+
+
+// const frase = "A turma do backend do Senac, é MASSA!";
+// const frase1 = "A turma do backend do Senac, é MASSA!".split('').reverse().join('');
+
+// Const temanho = frase.length
+
+// console.log (temanho);
+// console.log (frase1);
+
+
+// const nome = input.question("Digite o nome do aluno: ");
+// ver n1 = Nuber (input.question("digite nota 1: "));
+// ver n2 = Nuber (input.question("Digite nota 2: "));
+// ver n3 = Nuber (input.question("Digite nota 3: "));
+// ver n4 = Nuber (input.question("Digite nota 4: "));
+// ver n5 = Nuber (input.question("Digite nota 5: "));
+
+// const media = parseFloat(n1+n2+n3+n4+n5)/5
+
+// if ( media <= 3.5 ){
+//     console.log( nome + ", sua média foi, " + média + "Você  foi reprovado,")
+// } else if (média >= 3.6 && média <= 6.9){
+//     console.log( nome + ", sua média foi, " + média + "Você está em recuperação,")
+// } else{ 
+//     console.log( name + "sua média foi, " + média + " Você está aprovado")
+// }
